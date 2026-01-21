b9fe47cc7e4e68bbc5d72e0d9ee2bab90e6f8311
Godolphia <godolphia@Godolphias-MacBook-Pro.local>
2026-01-21 04:46:56 -0800

    logo adj

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="mb-4">
-        <img src="./public/logo512.png" />
+        <img src="./logo512.png" width="240" />
         <h1 className="font-weight-light display-1 text-center">
             Salty Kelp
         </h1>
