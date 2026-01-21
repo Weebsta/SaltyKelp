6b02a2ea0243bcb79417414a64ea6e01e5d95dce
Godolphia <godolphia@Godolphias-MacBook-Pro.local>
2026-01-21 04:50:51 -0800

    rm title text

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="mb-4">
-        <img src="./logo512.png" width="240" />
-        <h1 className="font-weight-light display-1 text-center">
-            Salty Kelp
-        </h1>
+        <img src="./logo512.png" width="240" alt="SaltyKelp" title="SaltyKelp" />
     </div>
   )
 }
