ad744b9a317b235035d80a21a28f42940dea9f3b
Godolphia <godolphia@Godolphias-MacBook-Pro.local>
2026-01-21 04:34:25 -0800

    title et al

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="mb-4">
+        <img src="./public/logo512.png" />
         <h1 className="font-weight-light display-1 text-center">
-            Restaurant Finder
+            Salty Kelp
         </h1>
     </div>
   )
