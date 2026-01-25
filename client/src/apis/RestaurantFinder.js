bb4ea02b84182799bc578bfd0faf345c457ea153
Godolphia <godolphia@Godolphias-MacBook-Pro.local>
2026-01-25 00:14:41 -0800

    updated db/index.js

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://saltykelp.com",
+    baseURL: "https://saltykelp.com/api/v1/restaurants",
 });
--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,8 @@
 // importing and destructuring Pool from pg;
 const { Pool } = require("pg");
+const dotenv = require('dotenv');
+
+dotenv.config();
  
 // using Pool to connect with our Postgres DB;
 const pool = new Pool({
@@ -8,6 +11,8 @@ const pool = new Pool({
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
  
 // exporting query function;
